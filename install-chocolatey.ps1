15af20ae6909cef56b5db58d4eed866271c91382
Mark Pitman <mark.pitman@gmail.com>
2020-03-13 15:19:29 -0700

    Split out chocolatey and other apps installation

---
+++ install-chocolatey.ps1
@@ -0,0 +1,3 @@
+# Installs chocolatey. Must be run from and administrative Windows Powershell (not Powershell Core) :(
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
--- setup.ps1
+++ setup.ps1
@@ -1,9 +1,3 @@
-
-
-# First install chocolatey. Must be run from and administrative Windows Powershell (not Powershell Core) :(
-
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
 choco install git -y --params="'/GitAndUnixToolsOnPath /NoAutoCrlf'"
 choco install notepadplusplus
 choco install vscode
