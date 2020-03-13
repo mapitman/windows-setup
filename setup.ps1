dd6cd32c9b903f384d36fb9e358f75557ecfd539
Mark Pitman <mark.pitman@gmail.com>
2020-03-13 15:22:35 -0700

    Add -y to all installs

--- setup.ps1
+++ setup.ps1
@@ -1,15 +1,15 @@
 choco install git -y --params="'/GitAndUnixToolsOnPath /NoAutoCrlf'"
-choco install notepadplusplus
-choco install vscode
-choco install firefox
-choco install chrome
-choco install vlc
-choco install audacity
-choco install 7zip
-choco install powershell-core
-choco install gpmdp
-choco install obs-studio
-choco install telegram
-choco install slack
-choco install geforce-experience
-choco install steam
+choco install notepadplusplus -y
+choco install vscode -y
+choco install firefox -y
+choco install chrome -y
+choco install vlc -y
+choco install audacity -y
+choco install 7zip -y
+choco install powershell-core -y
+choco install gpmdp -y
+choco install obs-studio -y
+choco install telegram -y
+choco install slack -y
+choco install geforce-experience -y
+choco install steam -y
